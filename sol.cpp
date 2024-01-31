8737e5db448e56afd42b469c99e6124c47eac67e
sulthanftr <sulthanftr@gmail.com>
2024-01-31 11:34:31 +0700

    init

---
+++ sol.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <climits>
+#include <functional>
+#include <queue>
+#include <string>
+#include <iterator>
+#include <algorithm>
+#include <set>
+#include <cmath>
+#include <utility>
+using namespace std;
+
+#define ll long long
+
+const int INF = 1e9;
+
+void solve() {
+
+}
+
+int main() {
+	//int t;
+    //cin >> t;
+    //while (t--)
+        solve();
+}
+
+
